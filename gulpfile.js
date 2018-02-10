cfda78a1cdc51b63e423513d7c29872b5202c8ab
adam <adamluptakosice@gmail.com>
2018-02-10 22:55:58 +0100

    finished client prototype node js mock up

--- gulpfile.js
+++ gulpfile.js
@@ -1,142 +1,157 @@
 var gulp = require('gulp'),
-gulp = require('gulp'),
-htmlmin = require('gulp-htmlmin'),
-fs = require('fs'),
-uglify = require('gulp-uglify'),
-clean = require('gulp-clean'),
-runSequence = require('run-sequence'),
-livereload = require('gulp-livereload'),
-server = require('gulp-server-livereload'),
-sass = require('gulp-sass'),
-inject = require('gulp-inject'),
-webserver = require('gulp-webserver'),
-htmlclean = require('gulp-htmlclean'),
-cleanCSS = require('gulp-clean-css'),
-concat = require('gulp-concat'),
-uglify = require('gulp-uglify'),
-del = require('del'),
-jshint = require('gulp-jshint'),
-replace = require('gulp-replace'),
-fail = require('gulp-fail'),
-gulpIf = require('gulp-if'),
-jshint = require('gulp-jshint'),
-jscs = require('gulp-jscs'),
-jscsStylish = require('gulp-jscs-stylish'),
-concat = require('gulp-concat');
+    gulp = require('gulp'),
+    htmlmin = require('gulp-htmlmin'),
+    fs = require('fs'),
+    uglify = require('gulp-uglify'),
+    clean = require('gulp-clean'),
+    runSequence = require('run-sequence'),
+    livereload = require('gulp-livereload'),
+    server = require('gulp-server-livereload'),
+    sass = require('gulp-sass'),
+    inject = require('gulp-inject'),
+    webserver = require('gulp-webserver'),
+    htmlclean = require('gulp-htmlclean'),
+    cleanCSS = require('gulp-clean-css'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    del = require('del'),
+    jshint = require('gulp-jshint'),
+    replace = require('gulp-replace'),
+    fail = require('gulp-fail'),
+    gulpIf = require('gulp-if'),
+    jshint = require('gulp-jshint'),
+    jscs = require('gulp-jscs'),
+    jscsStylish = require('gulp-jscs-stylish'),
+    jsEscape = require('gulp-js-escape'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat');
 
 var paths = {
-	src: 'src/**/*',
-	srcHTML: 'src/**/*.html',
-	srcCSS: 'src/**/*.css',
-	srcSASS:'src/sass/**/*.scss',
-	srcJS: 'src/**/*.js',
-	tmp: 'tmp',
-	tmpIndex: 'tmp/index.html',
-	tmpCSS: 'tmp/**/*.css',
-	tmpJS: 'tmp/**/*.js',
-	dist: 'dist',
-	distIndex: 'dist/index.html',
-	distCSS: 'dist/**/*.css',
-	distJS: 'dist/**/*.js'
+    src: 'src/**/*',
+    srcHTML: 'src/**/*.html',
+    srcCSS: 'src/**/*.css',
+    srcSASS: 'src/sass/**/*.scss',
+    srcJS: 'src/**/*.js',
+    tmp: 'tmp',
+    tmpIndex: 'tmp/index.html',
+    tmpCSS: 'tmp/**/*.css',
+    tmpJS: 'tmp/**/*.js',
+    dist: 'dist',
+    distIndex: 'dist/index.html',
+    distCSS: 'dist/**/*.css',
+    distJS: 'dist/**/*.js'
 };
 
-gulp.task('html', function () {
-	return gulp.src(paths.srcHTML).pipe(gulp.dest(paths.tmp));
+gulp.task('html', function() {
+    return gulp.src(paths.srcHTML).pipe(gulp.dest(paths.tmp));
 });
 
-gulp.task('css',['sass'], function () {
-	return gulp.src(paths.srcCSS).pipe(gulp.dest(paths.tmp));
+gulp.task('css', ['sass'], function() {
+    return gulp.src(paths.srcCSS).pipe(gulp.dest(paths.tmp));
 });
 
-gulp.task('js',['lint'], function () {
-	return gulp.src(paths.srcJS)
-	.pipe(gulp.dest(paths.tmp));
+gulp.task('js', ['lint'], function() {
+    return gulp.src(paths.srcJS)
+        .pipe(gulp.dest(paths.tmp));
 });
 
 gulp.task('copy', ['html', 'css', 'js']);
 
-gulp.task('inject', ['copy'], function () {
-	var css = gulp.src(paths.tmpCSS);
-	var js = gulp.src(paths.tmpJS);
-	return gulp.src(paths.tmpIndex)
-	.pipe(inject( css, { relative:true } ))
-	.pipe(inject( js, { relative:true } ))
-	.pipe(gulp.dest(paths.tmp));
+gulp.task('inject', ['copy'], function() {
+    var css = gulp.src(paths.tmpCSS);
+    var js = gulp.src(paths.tmpJS);
+    return gulp.src(paths.tmpIndex)
+        .pipe(inject(css, { relative: true }))
+        .pipe(inject(js, { relative: true }))
+        .pipe(gulp.dest(paths.tmp));
 });
 
-gulp.task('serve', ['inject'], function () {
-	return gulp.src(paths.tmp)
-	.pipe(webserver({
-		port: 3000,
-		livereload: true,
-		open: true
-	}));
+gulp.task('serve', ['inject'], function() {
+    return gulp.src(paths.tmp)
+        .pipe(webserver({
+            port: 3000,
+            livereload: true,
+            open: true
+        }));
 });
 
-gulp.task('watch', ['serve'], function () {
-	gulp.watch(paths.src, ['inject']);
+gulp.task('watch', ['serve'], function() {
+    gulp.watch(paths.src, ['inject']);
 });
 
 gulp.task('default', ['watch']);
 
-gulp.task('html:dist', function () {
-	return gulp.src(paths.srcHTML)
-	.pipe(htmlclean())
-	.pipe(htmlmin({collapseWhitespace: true}))
-	.pipe(gulp.dest(paths.dist));
+gulp.task('html:dist', function() {
+    return gulp.src(paths.srcHTML)
+        .pipe(htmlclean())
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest(paths.dist));
 });
-gulp.task('css:dist',['sass'], function () {
-	return gulp.src(paths.srcCSS)
-	.pipe(concat('style.min.css'))
-	.pipe(cleanCSS())
-	.pipe(gulp.dest(paths.dist));
+gulp.task('css:dist', ['sass'], function() {
+    return gulp.src(paths.srcCSS)
+        .pipe(concat('style.min.css'))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest(paths.dist));
 });
-gulp.task('js:dist',['lint'], function () {
-	return gulp.src(paths.srcJS)
-	.pipe(concat('script.min.js'))
-	.pipe(uglify())
-	.pipe(gulp.dest(paths.dist));
+gulp.task('js:dist', ['lint'], function() {
+    return gulp.src(paths.srcJS)
+        .pipe(concat('script.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.dist));
 });
 
 gulp.task('copy:dist', ['html:dist', 'css:dist', 'js:dist']);
 
-gulp.task('inject:dist', ['copy:dist'], function () {
-	var js = fs.readFileSync(paths.dist + '/script.min.js', 'utf8');
-	var css = fs.readFileSync(paths.dist + '/style.min.css', 'utf8');
-	var cssSrc = gulp.src(paths.distCSS);
-	var jsSrc = gulp.src(paths.distJS);
-	return gulp.src(paths.distIndex)
-	.pipe(inject( cssSrc, { relative:true } ))
-	.pipe(inject( jsSrc, { relative:true } ))
-	.pipe(replace('<script src="script.min.js"></script>', '<script type="text/javascript">' + js +'</script>'))
-	.pipe(replace('<link rel="stylesheet" href="style.min.css">', '<style type="text/css">'+ css +'</style>'))
-	.pipe(htmlclean())
-	.pipe(gulp.dest(paths.dist));
+gulp.task('inject:dist', ['copy:dist'], function() {
+    var js = fs.readFileSync(paths.dist + '/script.min.js', 'utf8');
+    var css = fs.readFileSync(paths.dist + '/style.min.css', 'utf8');
+    var cssSrc = gulp.src(paths.distCSS);
+    var jsSrc = gulp.src(paths.distJS);
+    return gulp.src(paths.distIndex)
+        .pipe(inject(cssSrc, { relative: true }))
+        .pipe(inject(jsSrc, { relative: true }))
+        .pipe(replace('<script src="script.min.js"></script>', '<script type="text/javascript">' + js + '</script>'))
+        .pipe(replace('<link rel="stylesheet" href="style.min.css">', '<style type="text/css">' + css + '</style>'))
+        .pipe(htmlclean())
+        .pipe(gulp.dest(paths.dist));
 });
 
-gulp.task('sass', function () {
-	return gulp.src(paths.srcSASS)
-	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-	.pipe(gulp.dest('./src/css/'));
+gulp.task('sass', function() {
+    return gulp.src(paths.srcSASS)
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(gulp.dest('./src/css/'));
 });
 
 gulp.task('build', ['inject:dist']);
 
-gulp.task('clean', function () {
-	del([paths.tmp, paths.dist,paths.srcCSS]);
+gulp.task('clean', function() {
+    del([paths.tmp, paths.dist, paths.srcCSS]);
 });
 
 gulp.task('lint', function() {
-	var jscsFailed = false;
+    var jscsFailed = false;
 
-	return gulp.src(paths.srcJS)
-	.pipe(jshint())
-	.pipe(jscs())
-	.pipe(jscsStylish.combineWithHintResults())
-	.pipe(jshint.reporter('jshint-stylish'));
-	// .pipe(gulpIf(function(file) {
-	// 	return ((file.jscs && !file.jscs.success)
-	// 		|| (file.jshint && !file.jshint.success));
-	// }, fail("Linting finished with errors!", true)));
+    return gulp.src(paths.srcJS)
+        .pipe(jshint())
+        .pipe(jscs())
+        .pipe(jscsStylish.combineWithHintResults())
+        .pipe(jshint.reporter('jshint-stylish'));
+    // .pipe(gulpIf(function(file) {
+    // 	return ((file.jscs && !file.jscs.success)
+    // 		|| (file.jshint && !file.jshint.success));
+    // }, fail("Linting finished with errors!", true)));
 })
 
+gulp.task('escape-quotes', function() {
+    return gulp.src(paths.dist + '/index.html')
+        .pipe(jsEscape())
+        .pipe(gulp.dest(paths.dist));
+})
+
+gulp.task('inject-html-to-cpp', ['escape-quotes'], function() {
+    var distHtml = fs.readFileSync(paths.dist + '/index.html', 'utf8');
+    return gulp.src('C:\\Users\\adam\\git\\ecap\\arduino\\src\\main.cpp')
+        .pipe(replace('"html"', distHtml))
+        .pipe(rename('main-html.cpp'))
+        .pipe(gulp.dest('C:\\Users\\adam\\git\\ecap\\arduino\\src\\'));
+})
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,105 @@
 /*jslint node: true */
 'use strict';
-var CONTROLLER_BASE_URL = 'http://www.mocky.io';
-var CONTROLLER_GET_URL = CONTROLLER_BASE_URL + '/v2/5a75f6b92e000057006ab249';
-var GET_CONTROLLER_DATA_INTREVAL = 1000;
+var CONTROLLER_BASE_URL = 'http://localhost:8080/testing676/ecap/1.0.0';
+var CONTROLLER_URL = CONTROLLER_BASE_URL + '/controller-data';
+var PID_URL = CONTROLLER_BASE_URL + '/pid';
+var SET_POINT_URL = CONTROLLER_BASE_URL + '/set-point';
+var FETCH_CONTROLLER_DATA_INTREVAL = 1000;
+var temperatures = [];
+var TEMPERATURES_KEY = 'temperatures';
+var LOCAL_STORAGE_LIMIT = 200000;
+/* jshint undef: false */
+window.onload = function() { setTimeout(function() { document.body.style.opacity = '100'; }, 1000); };
 
 function startController() {
-	console.log('Start Ecap Controller');
+    console.log('Start Ecap Controller');
+    var request = new XMLHttpRequest();
+
+    request.open('POST', SET_POINT_URL);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.onload = function() {
+        if (request.status === 201) {
+            console.log('Set point udpated');
+        } else if (request.status !== 200) {
+            console.log('An error occurred during the transaction POST new setPoint');
+        }
+    };
+    var newSetPoint = document.getElementById('set-point-input').value;
+    request.send(JSON.stringify({ setPoint: newSetPoint }));
 }
 
 function saveDataToExcel() {
-	console.log('Saving data to excel');
+    console.log('Saving data to excel');
 }
 
-function getControllerData() {
-	console.log('Fetching data from: ' + CONTROLLER_GET_URL);
+function fetchControllerData() {
+    console.log('Fetching data from: ' + CONTROLLER_URL);
+    /* jshint undef: false */
+    var request = new XMLHttpRequest();
+    request.onload = function(e) {
+        var controllerData = e.target.response;
+        updateHtmlData(controllerData);
+        saveData(controllerData);
+    };
+    request.onerror = function() {
+        var header = document.getElementById('header');
+        header.innerHTML = 'An error occurred during the transaction<br>GET:' + CONTROLLER_URL;
+    };
+    request.open('GET', CONTROLLER_URL, true);
+    request.setRequestHeader('Accept', 'application/json');
+    request.responseType = 'json';
+    request.send();
 }
 
-var getControllerDataTimer = setInterval(getControllerData, GET_CONTROLLER_DATA_INTREVAL);
+function saveData(controllerData) {
+    var newTemperatures = controllerData.temperatures;
+    var temperaturesRow = '';
+    for (var i = 0; i < newTemperatures.length; i++) {
+        temperaturesRow = temperaturesRow.concat('"' + newTemperatures[i].value + '"');
 
-window.onbeforeunload = function() {
-	clearInterval(getControllerDataTimer);
-};
->>>>>>> 4fe9cbe2c9523fed3d5d1f03586f4dd0d653b6c9
+        if (i < newTemperatures.length - 1) {
+            temperaturesRow = temperaturesRow.concat(',');
+        }
+    }
+
+    temperatures.push(temperaturesRow);
+    upsertDataLocalStorage(temperaturesRow);
+}
+
+function upsertDataLocalStorage(temperaturesRow) {
+    if (TEMPERATURES_KEY in localStorage) {
+        var storedTemperatures = localStorage.getItem(TEMPERATURES_KEY);
+        var jsonTemperatures = JSON.parse(storedTemperatures);
+        if (jsonTemperatures.length >= LOCAL_STORAGE_LIMIT) {
+            localStorage.removeItem(TEMPERATURES_KEY);
+            jsonTemperatures = [];
+        }
+        jsonTemperatures.push(temperaturesRow);
+        localStorage.setItem(TEMPERATURES_KEY, JSON.stringify(jsonTemperatures));
+    } else {
+        var temperatures = Array(1).fill(temperaturesRow);
+        localStorage.setItem(TEMPERATURES_KEY, JSON.stringify(temperatures));
+    }
+}
+
+function updateHtmlData(controllerData) {
+    if (controllerData.pid.activate) {
+        document.getElementById('startButton').style.display = 'none';
+        document.getElementById('stopButton').style.display = 'inline';
+        document.getElementById('set-point-input').style.display = 'none';
+    } else {
+        document.getElementById('stopButton').style.display = 'none';
+        document.getElementById('startButton').style.display = 'inline';
+        document.getElementById('set-point-input').style.display = 'inline';
+    }
+
+    document.getElementById('header').innerHTML = 'ECAP Temperature Controller';
+    document.getElementById('set-point').innerHTML = 'Setpoint: ' + controllerData.pid.setPoint + '°C';
+    var temperatures = controllerData.temperatures;
+    for (var i = 0; i < temperatures.length; i++) {
+        var temperature = temperatures[i];
+        document.getElementById(temperature.name).innerHTML = temperature.name + ': ' + temperature.value + '°C';
+    }
+}
+
+var fetchControllerDataTimer = setInterval(fetchControllerData, FETCH_CONTROLLER_DATA_INTREVAL);
